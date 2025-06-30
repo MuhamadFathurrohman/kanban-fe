a19600c897aad81674eb8630c765c3689e66a1db
MuhamadFathurrohman <muhamadfathurrohman05@gmail.com>
2025-06-30 20:57:49 +0700

    first commit

---
+++ src/router.jsx
@@ -0,0 +1,220 @@
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import Login from "./views/Auth/Login";
+import ReqKanban from "./views/Kanban/ReqKanban";
+import ProtectedRoute from "./components/ProtectedRoute";
+import ReqForm from "./views/Kanban/ReqForm";
+import Users from "./views/Users/Users";
+import AddUser from "./views/Users/AddUser";
+import EditUser from "./views/Users/EditUser";
+import Layout from "./components/Layout";
+import Home from "./views/Home";
+import DetailReqKanban from "./views/Kanban/DetailReqKanban";
+import ApprovalReqKanban from "./views/ApprovalReqKanban";
+import Report from "./views/Report";
+import SignUp from "./views/Auth/Signup";
+import ForgotPassword from "./views/Auth/ForgotPassword";
+import Profile from "./views/Users/Profile";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="/login" replace />, // Route untuk login
+  },
+  {
+    path: "/login",
+    element: <Login />, // Route untuk login
+  },
+  {
+    path: "/signup",
+    element: <SignUp />, // Route untuk login
+  },
+  {
+    path: "/forgot-password",
+    element: <ForgotPassword />, // Route untuk login
+  },
+  {
+    path: "/user", // Semua route dengan layout
+    element: (
+      <ProtectedRoute>
+        <Layout layoutType="user" />
+      </ProtectedRoute>
+    ),
+    children: [
+      {
+        index: true, // Default route saat buka "/"
+        element: <Home />,
+      },
+      {
+        path: "home-user",
+        element: <Home />,
+      },
+      {
+        path: "request-kanban",
+        element: (
+          <ReqKanban
+            userType="user"
+            title="REQUEST KANBAN"
+            apiEndpoint="/kanban/all"
+            showCreateButton={true}
+          />
+        ),
+      },
+      {
+        path: "request-form",
+        element: <ReqForm />,
+      },
+      {
+        path: "detail-request",
+        element: <DetailReqKanban />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: "/user-lead", // Semua route dengan layout
+    element: (
+      <ProtectedRoute>
+        <Layout layoutType="userLead" />
+      </ProtectedRoute>
+    ),
+    children: [
+      {
+        index: true, // Default route saat buka "/"
+        element: <Home />,
+      },
+      {
+        path: "home-user-lead",
+        element: <Home />,
+      },
+      {
+        path: "reqkanban-user-lead",
+        element: (
+          <ReqKanban
+            userType="user-lead"
+            title="REQUEST KANBAN"
+            apiEndpoint="/kanban/pending"
+            showApproveReject={true}
+          />
+        ),
+      },
+      {
+        path: "approve-user-lead",
+        element: (
+          <ApprovalReqKanban navigationPath="/user-lead/detailreq-user-lead" />
+        ),
+      },
+      {
+        path: "detailreq-user-lead",
+        element: <DetailReqKanban />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: "/admin", // Semua route dengan layout
+    element: (
+      <ProtectedRoute>
+        <Layout layoutType="admin" />
+      </ProtectedRoute>
+    ),
+    children: [
+      {
+        index: true, // Default route saat buka "/"
+        element: <Home />,
+      },
+      {
+        path: "home-admin",
+        element: <Home />,
+      },
+      {
+        path: "kanbanreq-admin",
+        element: (
+          <ReqKanban
+            userType="admin"
+            title="KANBAN REQUEST"
+            apiEndpoint="/kanban/all"
+          />
+        ),
+      },
+      {
+        path: "detail-kanbanreq",
+        element: <DetailReqKanban />,
+      },
+      {
+        path: "users",
+        element: <Users />,
+      },
+      {
+        path: "add-users",
+        element: <AddUser />,
+      },
+      {
+        path: "edit-users",
+        element: <EditUser />,
+      },
+      {
+        path: "report-admin",
+        element: <Report />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: "/pc-lead", // Semua route dengan layout
+    element: (
+      <ProtectedRoute>
+        <Layout layoutType="pcLead" />
+      </ProtectedRoute>
+    ),
+    children: [
+      {
+        index: true, // Default route saat buka "/"
+        element: <Home />,
+      },
+      {
+        path: "home-pc-lead",
+        element: <Home />,
+      },
+      {
+        path: "kanbanreq-pc-lead",
+        element: (
+          <ReqKanban
+            userType="pc-lead"
+            title="REQUEST KANBAN"
+            apiEndpoint="/kanban/pending"
+            showApproveReject={true}
+          />
+        ),
+      },
+      {
+        path: "detailreq-pc-lead",
+        element: <DetailReqKanban />,
+      },
+      {
+        path: "approve-pc-lead",
+        element: (
+          <ApprovalReqKanban navigationPath="/pc-lead/detailreq-pc-lead" />
+        ),
+      },
+      {
+        path: "report-pc-lead",
+        element: <Report />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+    ],
+  },
+]);
+
+export default router;
---
+++ src/views/Users/Users.jsx
@@ -0,0 +1,198 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate, Link, useOutletContext } from "react-router-dom";
+import "../../sass/Users/Users/Users.css";
+import Plus from "../../assets/icons/plus.svg";
+import Trash from "../../assets/icons/trash.svg";
+import Edit from "../../assets/icons/edit.svg";
+import Search from "../../assets/icons/search.svg";
+import { FaArrowRight } from "react-icons/fa";
+import { FaArrowLeft } from "react-icons/fa";
+import API from "../../service/api";
+import { DEPARTMENT_MAP } from "../../utils/constants";
+import { LoaderTable } from "../../components/LoaderTable";
+
+export default function Users() {
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const { showConfirmDelete } = useOutletContext();
+  const [loading, setLoading] = useState(true);
+
+  const itemsPerPage = 10;
+
+  const navigate = useNavigate();
+
+  const loadData = async (page = 1, keyword = "") => {
+    try {
+      setLoading(true);
+      const response = await API.get("/user", {
+        params: {
+          page,
+          limit: itemsPerPage,
+          search: keyword,
+        },
+      });
+
+      const res = response.data;
+      setData(res.data);
+      setTotalPages(res.totalPages);
+    } catch (error) {
+      alert("Gagal memuat data user!", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    loadData(currentPage, search);
+  }, [currentPage, search]);
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+    setCurrentPage(1);
+  };
+
+  const handleEdit = (id_users) => {
+    localStorage.setItem("id_users", id_users);
+    navigate(`/admin/edit-users`);
+  };
+
+  const handleDelete = async (id_users) => {
+    setData((prev) => prev.filter((item) => item.id_users !== id_users));
+    await API.delete(`/user/${id_users}`);
+  };
+
+  const handleClickDelete = (user) => {
+    showConfirmDelete(user, () => handleDelete(user.id_users));
+  };
+
+  const indexOfFirstItem = (currentPage - 1) * itemsPerPage;
+
+  const paginatedData = data;
+
+  return (
+    <div className="users-admin">
+      <div className="header-admin">
+        <h2>
+          <strong>LIST USERS</strong>
+        </h2>
+        <Link to="/admin/add-users" className="add-users-btn-admin">
+          <img src={Plus} alt="Add Icon" />
+          Add Users
+        </Link>
+      </div>
+
+      <div className="search-box-admin">
+        <img src={Search} alt="Search Icon" className="img-search-admin" />
+        <input
+          type="text"
+          placeholder="Search by Name or Email"
+          value={search}
+          onChange={handleSearch}
+        />
+      </div>
+
+      <div className="table-container-admin">
+        <table>
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Name</th>
+              <th>Department</th>
+              <th>Role</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {loading ? (
+              <tr>
+                <td
+                  colSpan="7"
+                  className="empty-row"
+                  style={{ height: "100px" }}
+                >
+                  <LoaderTable />
+                </td>
+              </tr>
+            ) : paginatedData.length === 0 ? (
+              <tr>
+                <td colSpan="7" className="empty-row-admin">
+                  No data available.
+                </td>
+              </tr>
+            ) : (
+              paginatedData.map((user, index) => (
+                <tr key={user.id}>
+                  <td>{indexOfFirstItem + index + 1}</td>
+                  <td>{user.name}</td>
+                  <td>{DEPARTMENT_MAP[user.department.name]}</td>
+                  <td>{user.role}</td>
+                  <td>{user.email}</td>
+                  <td>{user.no_hp}</td>
+                  <td className="actions-admin">
+                    <button
+                      className="edit-btn-admin"
+                      onClick={() => handleEdit(user.id_users)}
+                      title="Edit"
+                    >
+                      <img
+                        src={Edit}
+                        alt="Edit Icon"
+                        className="img-action-admin"
+                      />
+                    </button>
+                    <button
+                      className="delete-btn-admin"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        handleClickDelete(user);
+                      }}
+                      title="Delete"
+                    >
+                      <img
+                        src={Trash}
+                        alt="Delete Icon-admin"
+                        className="img-action-admin"
+                      />
+                    </button>
+                  </td>
+                </tr>
+              ))
+            )}
+          </tbody>
+        </table>
+      </div>
+
+      <div className="pagination-admin">
+        <button
+          disabled={currentPage === 1}
+          onClick={() => setCurrentPage(currentPage - 1)}
+          className="pagination-button-admin previous"
+        >
+          <FaArrowLeft size={12} className="previous-icon" /> Previous
+        </button>
+        {[...Array(totalPages)].map((_, idx) => (
+          <button
+            key={idx + 1}
+            className={`pagination-button-admin ${
+              currentPage === idx + 1 ? "active" : ""
+            }`}
+            onClick={() => setCurrentPage(idx + 1)}
+          >
+            {idx + 1}
+          </button>
+        ))}
+        <button
+          disabled={currentPage === totalPages}
+          onClick={() => setCurrentPage(currentPage + 1)}
+          className="pagination-button-admin next"
+        >
+          Next <FaArrowRight size={12} className="next-icon" />
+        </button>
+      </div>
+    </div>
+  );
+}
